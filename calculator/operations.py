ed770231772054f58628de1c48b062e3cfa79568
yunal234 <yl234@njit.edu>
2024-10-22 22:11:05 -0400

    added docstrings

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,17 +1,21 @@
 # operations.py
-
+'''Contains basic operations for the calculator'''
 from decimal import Decimal
 
 def add(a,b):
+    '''Add function'''
     return Decimal(a) + Decimal(b)
 
 def subtract(a, b):
+    '''Subtract function'''
     return Decimal(a) - Decimal(b)
 
 def multiply(a, b):
+    '''Multiply function'''
     return Decimal(a) * Decimal(b)
 
 def divide(a, b):
+    '''Divide function. Return an error if the division has zero'''
     if b == 0:
-        raise Value Error("Cannot divide by zero")
+        raise ValueError("Cannot divide by zero")
     return Decimal(a) / Decimal(b)
