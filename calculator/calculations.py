10dc7083c12cce61c3663ad663d1b1146e773467
yunal234 <yl234@njit.edu>
2024-10-22 23:20:12 -0400

    added the history operations to call the methods in the ManageHistory class.

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -1,18 +1,36 @@
 # calculations.py
 '''Contains the Calculator class which manages the calculations and history.'''
+from calculator.history import ManageHistory
+
 class Calculator:
     '''Calculator class will perform the calculations and store the history.'''
     def __init__(self):
-        '''Initialize the empty history list.'''
-        self.history = []
+        '''Initialize history class.'''
+        self.history = ManageHistory()
 
     def execute(self, command):
         '''Execute the commands and store the history to result and 
         return the result of the executed command.'''
         result = command.execute()
-        self.history.append((command.__class__.__name__, command.a, command.b, result))
+        self.history.add_entry(command.__class__.__name__, command.a, command.b, result)
         return result
+
+    def show_history(self):
+        '''show the history'''
+        self.history.show_history()
+
+    def save_history(self):
+        '''save the history'''
+        self.history.save_history()
+
+    def load_history(self):
+        '''load the history'''
+        self.history.load_history()
+
+    def clear_history(self):
+        '''clear the history'''
+        self.history.load_history()
 
     def get_history(self):
         '''Get the history of the calculations and return the list of each calculation.'''
-        return self.history
+        return self.history.get_history()
