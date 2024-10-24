06d363c4380e47079ef1c5dfa34639d123ca8c0b
yunal234 <yl234@njit.edu>
2024-10-23 22:22:19 -0400

    added the command handler file to the commands folder. This file will manage the plugins.

---
+++ commands/command_handler.py
@@ -0,0 +1,35 @@
+# command_handler.py
+'''The CommandHandler class is responsible for loading and
+executing dynamically loaded plugins'''
+import os
+import importlib
+
+class CommandHandler:
+    '''This class will manage the plugis'''
+    def __init__(self, plugin_dir='plugins'):
+        '''Initialize the CommandHandler'''
+        self.plugin_dir = plugin_dir
+        self.plugins = {}
+
+    def load_plugins(self):
+        '''Load plugin from the plugin_dir.
+        Each plugin file should correspond with its specified attribute'''
+        # Look before you leap
+        for root, files in os.walk(self.plugin_dir):
+            for file in files:
+                if file.endswith('.py') and file != '__init__.py':
+                    module_name = f"{root.replace('/', '.')}.{file[:-3]}"
+                    module = importlib.import_module(module_name)
+
+                    if hasattr(module, 'COMMAND'):
+                        self.plugins[module.COMMAND] = module.AddCommand
+
+    def execute_command(self, operation, a,b):
+        '''Execute the command of the operation and return the result.
+        If there command is not found than return an error.'''
+        if operation in self.plugins:
+            command_class = self.plugins[operation]
+            command = command_class(a, b)
+            return command.execute()
+
+        raise ValueError(f"Unknown operation: {operation}")
