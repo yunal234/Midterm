50385d092519bc53ca1c4e9c06b2642990ed4c75
yunal234 <yl234@njit.edu>
2024-10-24 01:37:02 -0400

    correct the code to get the history to properly save.

--- calculator/history.py
+++ calculator/history.py
@@ -1,5 +1,5 @@
-# history.py
 '''History management to keep track of calculations'''
+import os
 import pandas as pd
 
 class ManageHistory:
@@ -8,12 +8,24 @@ class ManageHistory:
     def __init__(self, filename='history.csv'):
         '''Initialize the history manager with the file'''
         self.filename = filename
-        self.history_df = self.load_history()
+        self.history_df = pd.DataFrame(columns=['Operation', 'a', 'b', 'Result'])
+        if not os.path.exists(self.filename):
+            self.save_history()
+        else:
+            self.load_history()
+
+    def show_history(self):
+        '''Show the history'''
+        if self.history_df.empty:
+            print("No history entries found.")
+        else:
+            print("Displaying current history:")
+            print(self.history_df.to_string(index=False))
 
     def add_entry(self, operation, a, b, result):
         '''Add new calculation to the history'''
-        new_entry = {'Operation': operation, 'a': a, 'b': b, 'Result': result}
-        self.history_df = self.history_df.append(new_entry, ignore_index=True)
+        new_entry = pd.DataFrame([{'Operation': operation, 'a': a, 'b': b, 'Result': result}])
+        self.history_df = pd.concat([self.history_df, new_entry], ignore_index=True)
         self.save_history()
 
     def save_history(self):
@@ -23,19 +35,11 @@ class ManageHistory:
     def load_history(self):
         '''Load the history from the file. Create if file does not exist'''
         try:
-            return pd.read_csv(self.filename)
+            self.history_df = pd.read_csv(self.filename)
         except FileNotFoundError:
-            return pd.DataFrame(columns=['Operation', 'a', 'b', 'Result'])
-
-    def show_history(self):
-        '''show the history'''
-        print(self.history_df)
+            print("No history file found.")
+            self.history_df = pd.DataFrame(columns=['Operation', 'a', 'b', 'Result'])
 
     def clear_history(self):
         '''Clear the history of the file and save'''
         self.history_df = pd.DataFrame(columns=['Operation', 'a', 'b', 'Result'])
-        self.save_history()
-
-    def get_history(self):
-        '''return the history data frame'''
-        return self.history_df
