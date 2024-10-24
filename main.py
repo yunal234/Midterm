882e7d95a45faccbdcf18303544081f2d788de43
yunal234 <yl234@njit.edu>
2024-10-23 22:24:03 -0400

    refactored the main file to integrate command handler and plugins

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 # main.py
 '''REPL interface for user interaction'''
-from commands.add import AddCommand
-from commands.subtract import SubtractCommand
-from commands.multiply import MultiplyCommand
-from commands.divide import DivideCommand
 from calculator.calculations import Calculator
+from commands.command_handler import CommandHandler
 
-def show_menu():
+def show_menu(handler):
     '''Display the menu with list of operations'''
-    print("\nChoose an operation: Add, Subtract, Multiply, Divide")
-    print("Type 'history' to see calculation history.")
-    print("Type 'save' to save history, 'load' to load history, 'clear' to clear the history.")
+    print("\nChoose an operation:")
+    for command in handler.plugins:
+        print(f"- {command.capitalize()} (plugin)")
+    print("Type 'history' to see your calculation history.")
     print("Type 'exit' to quit the calculator application.")
 
 def process_history_commands(operation, calculator):
@@ -32,9 +30,11 @@ def process_history_commands(operation, calculator):
 def main():
     '''Run the calculator REPL loop'''
     calculator = Calculator()
+    handler = CommandHandler()
+    handler.load_plugins()
 
     while True:
-        show_menu()
+        show_menu(handler)
         operation = input("Enter operation: ").strip().lower()
 
         if operation == 'exit':
@@ -48,19 +48,7 @@ def main():
         b = input("Enter second number: ").strip()
 
         try:
-            if operation == 'add':
-                command = AddCommand(a, b)
-            elif operation == 'subtract':
-                command = SubtractCommand(a, b)
-            elif operation == 'multiply':
-                command = MultiplyCommand(a, b)
-            elif operation == 'divide':
-                command = DivideCommand(a, b)
-            else:
-                print("Unknown Operation. Please choose an operation from the menu")
-                continue
-
-            result = calculator.execute(command)
+            result = handler.execute_command(operation, a, b)
             print(f"Result: {result}")
 
         except ValueError as e:
